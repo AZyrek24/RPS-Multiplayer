e75a121525aed881e0d223108da38792705d341f
Anthony Zyrek <anthonyzyrek@Anthonys-MacBook-Pro.local>
2018-06-09 20:46:00 -0700

    basic styling template for game

---
+++ assets/javascript/rps.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBwNI0v7FDNPuzhibpNnSTSOJKatGSkLEs",
+  authDomain: "rps-multiplayer-ba423.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-ba423.firebaseio.com",
+  projectId: "rps-multiplayer-ba423",
+  storageBucket: "",
+  messagingSenderId: "798644746918"
+};
+firebase.initializeApp(config);
