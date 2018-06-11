9a8231a2abace9a2dda61b863462eb0a4ac6d657
Anthony Zyrek <anthonyzyrek@Anthonys-MacBook-Pro.local>
2018-06-11 14:59:47 -0700

    rps.js button stores id variable

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -1,3 +1,4 @@
+
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyBwNI0v7FDNPuzhibpNnSTSOJKatGSkLEs",
@@ -8,3 +9,18 @@ var config = {
   messagingSenderId: "798644746918"
 };
 firebase.initializeApp(config);
+
+// Variables
+var playerId = "";
+var numPlayers = 2;
+var player1wins = 0;
+var player2wins = 0;
+var ties = 0;
+var gameUrl = "https://rps-multiplayer-ba423.firebaseio.com";
+
+$(document).ready(function () {
+  $("button").on("click", function () {
+    playerId = $("#player-name").val().trim();
+  })
+  
+})
