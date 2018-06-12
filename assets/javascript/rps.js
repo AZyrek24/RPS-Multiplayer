43cd21517e828d17540a3f61b9a63dff6fbe664d
Anthony Zyrek <anthonyzyrek@Anthonys-MacBook-Pro.local>
2018-06-11 17:13:55 -0700

    function added to check if there is enough players

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -1,14 +1,15 @@
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyBwNI0v7FDNPuzhibpNnSTSOJKatGSkLEs",
-  authDomain: "rps-multiplayer-ba423.firebaseapp.com",
-  databaseURL: "https://rps-multiplayer-ba423.firebaseio.com",
-  projectId: "rps-multiplayer-ba423",
+  apiKey: "AIzaSyBaluwB-Ti4pMYbQi_5GP9P7WQar3gwEjg",
+  authDomain: "rps-multiplayer-f26ec.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-f26ec.firebaseio.com",
+  projectId: "rps-multiplayer-f26ec",
   storageBucket: "",
-  messagingSenderId: "798644746918"
+  messagingSenderId: "912188291755"
 };
 firebase.initializeApp(config);
+
 
 // Variables
 var playerId = "";
@@ -16,11 +17,29 @@ var numPlayers = 2;
 var player1wins = 0;
 var player2wins = 0;
 var ties = 0;
-var gameUrl = "https://rps-multiplayer-ba423.firebaseio.com";
+var currentNumPlayers = 0;
+var gameUrl = "https://rps-multiplayer-f26ec.firebaseio.com";
 
 $(document).ready(function () {
-  $("button").on("click", function () {
+
+  $("#start-button").on("click", function () {
+    var database = firebase.database();
     playerId = $("#player-name").val().trim();
+    database.ref().set({
+      player1: playerId
+    });
+    currentNumPlayers++;
+    checkNumPlayers();
+    console.log(currentNumPlayers);
   })
-  
-})
+
+  function checkNumPlayers() {
+    if (currentNumPlayers >= numPlayers) {
+      alert("game full");
+      startGame();
+    }
+  }
+  function startGame() {
+    alert("ready");
+  }
+});
