fec8c2078fb6a470cb6eafc875d91a42697fe3b3
Anthony Zyrek <anthonyzyrek@Anthonys-MacBook-Pro.local>
2018-06-14 17:22:21 -0700

    reset rps.js to funcitoning state

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -1,87 +1,76 @@
-
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyBaluwB-Ti4pMYbQi_5GP9P7WQar3gwEjg",
-  authDomain: "rps-multiplayer-f26ec.firebaseapp.com",
-  databaseURL: "https://rps-multiplayer-f26ec.firebaseio.com",
-  projectId: "rps-multiplayer-f26ec",
-  storageBucket: "",
-  messagingSenderId: "912188291755"
-};
-firebase.initializeApp(config);
-
-
-// Variables
-var playerId = "";
-var numPlayers = 2;
-var player1wins = 0;
-var player2wins = 0;
-var player1losses = 0;
-var player2losses = 0;
-var ties = 0;
-var currentNumPlayers = 0;
-var gameUrl = "https://rps-multiplayer-f26ec.firebaseio.com";
-
 $(document).ready(function () {
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBaluwB-Ti4pMYbQi_5GP9P7WQar3gwEjg",
+    authDomain: "rps-multiplayer-f26ec.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-f26ec.firebaseio.com",
+    projectId: "rps-multiplayer-f26ec",
+    storageBucket: "",
+    messagingSenderId: "912188291755"
+  };
+  firebase.initializeApp(config);
+
+
+  // Variables
+  var playerId1 = "";
+  var playerId2 = "";
+  var numPlayers = 2;
+  var player1wins = 0;
+  var player2wins = 0;
+  var player1losses = 0;
+  var player2losses = 0;
+  var ties = 0;
+  var currentNumPlayers = 0;
+  
+  var gameUrl = "https://rps-multiplayer-f26ec.firebaseio.com";
+
+  //Firebase database call
   var database = firebase.database();
 
   $("#start-button").on("click", function () {
+
     if (currentNumPlayers === 0) {
       database = firebase.database();
-      playerId = $("#player-name").val().trim();
-      database.ref().update({
-        players: {
-          1: {
-            player1: playerId,
-          }
-        }
+      playerId1 = $("#player-name").val().trim();
+      database.ref('users/' + playerId1).set({
+        wins: 0,
+        losses: 0,
+        chat: ""
       });
-      $("#player-1").text("Player 1: " + playerId.toUpperCase());
+      $("#player-1").text("Player 1: " + playerId1.toUpperCase());
       currentNumPlayers++;
       checkNumPlayers();
-      console.log(currentNumPlayers);
+      $("form").get(0).reset()
+      $("#box-center").html("Waiting for a Second Player...");
     }
-    else {
+    else if (currentNumPlayers === 1) {
       database = firebase.database();
-      playerId = $("#player-name").val().trim();
-      database.ref().update({
-        players: {
-          2: {
-            player2: playerId,
-          }
-        }
+      playerId2 = $("#player-name").val().trim();
+      database.ref('users/' + playerId2).set({
+        wins: 0,
+        losses: 0,
+        chat: ""
       });
-      $("#player-2").text("Player 2: " + playerId.toLocaleUpperCase());
+      $("#player-2").text("Player 2: " + playerId2.toLocaleUpperCase());
       currentNumPlayers++;
       checkNumPlayers();
-      console.log(currentNumPlayers);
+      $("#box-center").html("LETS GET IT ON!!!");
+      $("#start-row").empty();
     }
+
   })
 
   $("#chat-button").on("click", function () {
     database = firebase.database();
     var chatText = $("#chat-input").val().trim();
-    database.ref().set({
-      chat: chatText
-    });
+    $("#message-display").html("<h5>" + chatText + "</h5>");
   })
 
-  database.ref().on("child_added", function (childSnapshot) {
-
-    // Log everything that's coming out of snapshot
-    console.log(childSnapshot.val().chat);
-
-    // full list of items to the well
-    $("#message-display").append(childSnapshot.val().chat);
-
-    // Handle the errors
-  }, function (errorObject) {
-    console.log("Errors handled: " + errorObject.code);
-  });
+  
 
   function checkNumPlayers() {
     if (currentNumPlayers === numPlayers) {
-      alert("game full");
       startGamePlayer1();
       startGamePlayer2();
     }
@@ -91,7 +80,7 @@ $(document).ready(function () {
     $("#box-left").append("<br><h5 id='paper1'>Paper</h5>");
     $("#box-left").append("<br><h5 id='scissors1'>Scissors</h5>");
     $("#box-left").append("<br><h6 id='wins-loss-1'>Wins: " + player1wins + " Losses: " + player1losses)
-    
+
   }
   function startGamePlayer2() {
     $("#box-right").append("<br><h5 id='rock2'>Rock</h5>");
